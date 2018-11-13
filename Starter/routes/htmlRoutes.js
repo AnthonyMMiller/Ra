dcbdd368048f91177628121bd7af4302e285e56d
Olevina <olevina@ymail.com>
2018-11-13 16:43:35 -0700

    User page + polished Main

--- Starter/routes/htmlRoutes.js
+++ Starter/routes/htmlRoutes.js
@@ -20,6 +20,15 @@ module.exports = function(app) {
     });
   });
 
+    // Load example page and pass in an example by id
+    app.get("/user", function(req, res) {
+      db.Example.findOne({}).then(function(dbExample) {
+        res.render("user", {
+          example: dbExample
+        });
+      });
+    });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
