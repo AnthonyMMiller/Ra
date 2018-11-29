25e0c0a518f98f8664b932884c41973a9f17d3ce
Olevina <olevina@ymail.com>
2018-11-29 12:14:30 -0700

    feelbad

--- public/js/user.js
+++ public/js/user.js
@@ -25,6 +25,7 @@ function GetData (Jdata ){
   Sleep = Math.floor((test.Sleep/test.total)*100);
   Anxiety = Math.floor((test.Anxiety/test.total)*100);
   Increased = Math.floor((test.Increased/test.total)*100);
+  Fine = Math.floor((test.Fine/test.total)*100);
   console.log("this should be my  obj with roma #" + test.Headaches);
   
   $(".stats").append(romaP +"% of users are feeling Headaches or migraines today");
@@ -39,6 +40,8 @@ function GetData (Jdata ){
   $(".stats").append(Anxiety +"% of users are feeling Anxiety today");
   $(".stats").append("<br>");
   $(".stats").append(Increased +"% of users are feeling Increased sensitivity today");
+  $(".stats").append("<br>");
+  $(".stats").append(Fine +"% of users are feeling Fine today");
  
 };
 
@@ -51,6 +54,7 @@ function getStats(feeling){
   Sleep:0,
   Anxiety:0,
   Increased:0,
+  Fine:0,
    total:0
  };
  feeling.forEach(x => {
@@ -77,6 +81,10 @@ function getStats(feeling){
 
     case"Increased sensitivity": 
       data.Increased++
+      break;
+
+    case"Fine": 
+      data.Fine++
       break;
   }
  });
