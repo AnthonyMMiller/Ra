3682c223b4bb5435398e03e46bab8702afec5581
wherding <wherding@outlook.com>
2018-11-28 19:57:36 -0700

    added nasa api data to database

--- Starter/models/user.js
+++ Starter/models/user.js
@@ -1,6 +1,7 @@
 module.exports = function (sequelize, DataTypes) {
   var UserFeeling = sequelize.define("UserFeeling", {
     feeling: DataTypes.STRING,
+    nasa: DataTypes.STRING(1235),
   });
   return UserFeeling;
 };
